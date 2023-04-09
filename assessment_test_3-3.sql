461329a2483c12fc01ea6d500af6d2b6355c9b88
RafaelSampaioMoura <rafaelsm123@outlook.com>
2023-04-09 13:14:35 -0300

    Assessment Test 3 Question 3

---
+++ assessment_test_3-3.sql
@@ -0,0 +1,12 @@
+INSERT INTO 
+	students(first_name, 
+			 last_name, 
+			 homeroom_number, 
+			 phone, 
+			 graduation_year)
+VALUES 
+	('Mark',
+	'Watney',
+	'5',
+	'777-555-1234',
+	'2035-11-27');
