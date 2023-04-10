9a1de9069d37f091d547e70ac8e633964f6b3904
RafaelSampaioMoura <rafaelsm123@outlook.com>
2023-04-10 18:41:42 -0300

    Create table for customers

---
+++ create-customers-table.sql
@@ -0,0 +1,9 @@
+USE sales;
+CREATE TABLE customers
+(
+	customer_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email_address VARCHAR(255) UNIQUE NOT NULL,
+    number_of_complaints INT
+);
