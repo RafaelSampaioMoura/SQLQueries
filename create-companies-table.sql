f2c7b9a17ba2b34ae7cd44478b9c3785b78475a3
RafaelSampaioMoura <rafaelsm123@outlook.com>
2023-04-11 15:23:03 -0300

    Update script for creating companies table on sales database

--- create-companies-table.sql
+++ create-companies-table.sql
@@ -3,7 +3,11 @@ DROP TABLE IF EXISTS companies;
 
 CREATE TABLE companies
 (
-	company_id VARCHAR(255) NOT NULL PRIMARY KEY,
-    company_name VARCHAR(255) NOT NULL UNIQUE,
-    headquarters_phone_number INT
+	company_id VARCHAR(255),
+    company_name VARCHAR(255) DEFAULT 'X',
+    headquarters_phone_number VARCHAR(255),
+PRIMARY KEY(company_id),
+UNIQUE KEY(headquarters_phone_number)
 );
+
+DROP TABLE companies;
